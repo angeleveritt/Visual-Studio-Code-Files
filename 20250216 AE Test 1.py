bcaa70f1a7bfc3036464ff134102e9fdd716f7ab
angeleveritt <angel.everitt@gmail.com>
2025-02-16 17:59:56 -0500

    prints file name at top

---
+++ 20250216 AE Test 1.py
@@ -0,0 +1,20 @@
+#var_name = open("file_name", "mode")
+message = "Welcome to Python Guides"
+file = open(Greeting.txt", "w")
+file.write(message)
+file.close()
+print("Successfully Created a file")
+#
+#
+#with open(file_name, "x") as "alias name"
+#
+#
+name=input("Enter your name:")
+user_name=input("Enter username:")
+password=input(""Enter the password:")
+file_name- name+."txt"+a"with open(file_name, 'x") as file:
+  un=file.write(f"Username:{user_name}")
+  passwd=file.write(f"\nPassword:{password}")
+  #
+  #
+print("Account created successfully!!")
