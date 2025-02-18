cca11aa9338de07da1355abd6956dcee76e6b48f
angeleveritt <angel.everitt@gmail.com>
2025-02-18 17:06:34 -0500

    not sure if any change

---
+++ various/playing_iterationandindex.py
@@ -0,0 +1,13 @@
+# playing w iteration and an index
+print()
+print("playing with iteration and an index")
+print()
+
+ls = [7, "Hello", 2.5]
+
+for ind in range(len(ls)):  #iterate over a list
+    print("index:", ind, "item:", ls[ind])
+
+print()
+print("fin")
+print()
