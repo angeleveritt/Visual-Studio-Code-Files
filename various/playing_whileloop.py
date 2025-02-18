d06100c6037393953ed4f642ec2b6ba7101bc381
angeleveritt <angel.everitt@gmail.com>
2025-02-18 16:49:52 -0500

    initial

---
+++ various/playing_whileloop.py
@@ -0,0 +1,25 @@
+# While Loop exercise
+print()
+print("Playing w the While Loop where the condition is (i > 0)")
+print()
+
+i = 5
+
+while i > 0:
+    print("i =", i)
+    i -= 1
+    
+print()    
+print("Goodbye")
+print()
+print("While ... else statement where condition is (num > 5)")
+num = 10
+print("The number is", num)
+print()
+while num > 5:
+    print("Inside the loop, num:", num)
+    num -= 1
+else:
+    print("Outside the loop, num:", num)
+
+print()
