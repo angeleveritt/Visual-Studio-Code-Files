60cfc6a8fd080bc5b25a66878d9f6eeaae5e9f32
angeleveritt <angel.everitt@gmail.com>
2025-02-18 12:10:04 -0500

    elif working

---
+++ various/playing_elif.py
@@ -0,0 +1,27 @@
+# playing w elif
+print()
+print("Angel playing w elif")
+print()
+
+grade = 95
+#should make this an input program
+
+print("Grade is", grade)
+if grade > 85:
+    print("Excellent")
+elif grade > 75:
+    print("Very good")
+elif grade > 65:
+    print("Good")
+elif grade > 50:
+    print("Pass")
+
+else:
+    print("Failed")
+
+print()
+
+print("Good luck with the rest of the course")
+
+print()
+
