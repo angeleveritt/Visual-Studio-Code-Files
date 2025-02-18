3e709bceba3a00540322eeba8e8c2a61f48c7e30
angeleveritt <angel.everitt@gmail.com>
2025-02-18 11:54:17 -0500

    initial

---
+++ various/ifthen.py
@@ -0,0 +1,19 @@
+# playing with if then
+
+print()
+print("playing w if then")
+a = 30
+
+print("Checking first condition")
+if a>20: 
+    print("a is greater than 20")
+    # can also be written on one line
+    #if a>20: print("a is greater than 20")
+
+print("Checking second condition")
+if a<20:
+    print("a is less than 20")
+
+print("Bye bye")
+
+      
