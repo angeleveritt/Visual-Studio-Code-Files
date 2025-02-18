2a082fa8b887ec5b80d85751a33f619c9947c653
angeleveritt <angel.everitt@gmail.com>
2025-02-17 20:10:05 -0500

    moved it to separate folder

---
+++ CalculateAge/main.py
@@ -0,0 +1,15 @@
+# main.py for UWO Calculate Age mini project - by Angelique Everitt
+
+start_year = int(input("Enter your birth year: "))
+endyear = int(input("Enter the current year: "))
+age = (endyear - start_year)
+print ("Your age this year is or will be: ", age)
+
+#int(start_year)  ## could be a separate line
+#int(end-year)  ## could be a separate line
+
+
+
+
+
+
