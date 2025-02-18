ddccc998581ba10491a6f21859dd49807ea25b56
angeleveritt <angel.everitt@gmail.com>
2025-02-18 12:52:45 -0500

    initial

---
+++ various/playing_remainder.py
@@ -0,0 +1,19 @@
+# calculating remainder
+print()
+
+num = 21
+remainder = num % 7
+
+print("The remainder is", remainder)
+
+if remainder > 0:
+    print(num, "is not divisible by 7")
+    divisible = False
+
+else:
+    divisible = True
+    print(num, "is divisible by 7")
+
+print()
+
+
