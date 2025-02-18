83d1888358e3b2bddca1f6f8e349e58318bb76b0
angeleveritt <angel.everitt@gmail.com>
2025-02-18 12:03:47 -0500

    if else exercises

---
+++ various/playing_ifelse.py
@@ -0,0 +1,30 @@
+# playing w if else
+
+print()
+print("playing w if else")
+print()
+
+a = 10
+print("a is", a)
+
+if a>20:
+    print("a is greater than 20")
+
+else:
+    print("a is not greater than 20")
+
+print()
+
+b = 4
+print("b is", b)
+
+if b>2:
+    print("b is greater than 2")
+
+else:
+    print("b is not greater than 2")
+
+print()
+
+
+    
