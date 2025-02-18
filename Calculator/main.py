8febfcb791630acf75c33268f3265ba28ba6905d
angeleveritt <angel.everitt@gmail.com>
2025-02-18 10:25:48 -0500

    Create main.py

---
+++ Calculator/main.py
@@ -0,0 +1,16 @@
+# main.py for UWO Calculator mini project - by Angelique Everitt
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+sum = (first_number + second_number)
+subtraction = (first_number - second_number)
+multiplication = (first_number * second_number)
+division = (first_number / second_number)
+print("first number + second number is ", sum)
+print("first number - second number is ", subtraction)
+print("first number * second number is ", multiplication)
+print("first number / second number ", division)
+
+
+
+
