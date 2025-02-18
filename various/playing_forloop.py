010e2eeafb663992ee9ab72f532979d42cd0f851
angeleveritt <angel.everitt@gmail.com>
2025-02-18 17:00:06 -0500

    initial

---
+++ various/playing_forloop.py
@@ -0,0 +1,16 @@
+# playing with For Loop
+print()
+print("Playing with the For Loop")
+print()
+ls = [7, "Hello", 2.5]
+st = "RoboGarden"
+
+for item in ls:  #iterate over a list
+    print("item:", item)
+
+for letter in st:
+    print("letter:", letter)
+
+print()
+print("fin")    
+print()
