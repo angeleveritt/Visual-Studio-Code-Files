0d1252150755d88e2cd45dedcd432070d50e83a9
angeleveritt <angel.everitt@gmail.com>
2025-02-18 12:23:00 -0500

    initial

--- various/playing_elif.py
+++ various/playing_elif.py
@@ -25,3 +25,25 @@ print("Good luck with the rest of the course")
 
 print()
 
+
+x = 22
+print("x =", x)
+
+if x > 20:
+    x += 5 # augmented assignment operators //  (x = x+1) can be written (x += 5) could also be x -= 5, x *= 5
+
+elif x > 15:
+    x += 4     
+
+elif x > 10:
+    x += 3
+
+else:
+    x += 2
+
+print(x)
+
+print()
+
+
+
