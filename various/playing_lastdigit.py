da16f4ba0424dc31fc0d2f733236ff2877c08573
angeleveritt <angel.everitt@gmail.com>
2025-02-18 16:22:37 -0500

    minor updates

--- various/playing_lastdigit.py
+++ various/playing_lastdigit.py
@@ -6,11 +6,11 @@ print()
 num = 27
 print("The number is", num)
 
-lastdigit = (27 % 10)
+lastdigit = (num % 10)
 print("The last digit is", lastdigit)
 
 if (lastdigit / 3) > 0:
-    remainder = False
+    remainder = False  #they used divisible for the variable in the exercise I submitted - I adjusted there but not here.
     print("The last digit is not divisible by 3")
 
 else:
