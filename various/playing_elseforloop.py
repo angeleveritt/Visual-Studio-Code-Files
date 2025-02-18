a7c97b8a932ee92251de1540ec910f506e5cd180
angeleveritt <angel.everitt@gmail.com>
2025-02-18 17:14:51 -0500

    initial

---
+++ various/playing_elseforloop.py
@@ -0,0 +1,30 @@
+#playing with else and for loop
+print()
+print("playing with else and for loop")
+print()
+st = "Robo"
+for ind in range(len(st)):
+    print("ind:", ind, "letter:", st[ind])
+else:
+    print("No other letters in st:")
+
+print()
+[print("fin")]
+print()
+
+#another exercise
+print()
+print("another iterative exercise")
+print()
+x = 0
+print(x)
+ls=[2, 8, 9]
+for item in ls:
+    x += item
+    print(x)
+print(x)
+print()
+print("fin")
+print()
+
+
