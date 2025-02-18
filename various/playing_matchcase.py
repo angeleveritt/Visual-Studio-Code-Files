a806ceb0f7854b3de53d6cb354b836941f607e7e
angeleveritt <angel.everitt@gmail.com>
2025-02-18 12:43:04 -0500

    initial

--- various/playing_matchcase.py
+++ various/playing_matchcase.py
@@ -20,4 +20,17 @@ print("fin")
 print()
 
 
+match command.split():
+    case ['show']:
+        print('List all files and directories: ')
+        #code to list files
+    case ['remove', *files]:
+        print('Removing files: {}'.format(files))
+        #code to reove files
+    case _:
+        print('Command not recognized')
+
+print()
+
+
 
