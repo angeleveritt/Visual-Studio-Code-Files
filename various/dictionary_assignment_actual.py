33a23d9cb837b0450c0b6c18bdadaae8beb83479
angeleveritt <angel.everitt@gmail.com>
2025-02-24 15:17:37 -0500

    initial

---
+++ various/dictionary_assignment_actual.py
@@ -0,0 +1,32 @@
+#dictionary & list assignment
+print()
+print("dictionary & list assignment")
+print()
+ls = [10,2,7,23,5,11,15]
+print(ls)
+max = max(ls)
+print("max is : ", max)
+min = min(ls)
+print("min is : ", min)
+dict_out = {"min": min, "max": max}
+print("dict_out contains : ", dict_out)
+
+print()
+print("fin")
+print()
+
+#def main():
+#    for index in range(len(ls)):
+#        print(f"index {index}: {ls[index}")
+#        if ls[i] > max:
+#            max = ls[i]
+#        elif ls[i] < min:
+#            min = ls[i]
+  
+#    dict_out = {"min": ls[], "max": ls[]}
+
+
+#    print(dict_out)
+#    return(dict_out)
+
+#main()
