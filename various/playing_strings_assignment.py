cb7b7e8deb206cc32c2bbd3ce7bf0bbb7f5ef027
angeleveritt <angel.everitt@gmail.com>
2025-02-24 13:31:04 -0500

    initial

---
+++ various/playing_strings_assignment.py
@@ -0,0 +1,15 @@
+# playing with strings
+print("string operators assignment")
+print()
+st = "Kindly check your code.  Your code looks interesting"
+print(st)
+first2 = st[0:2]
+last2 = st[-2:]
+st_out = first2+last2
+temp = (st_out*2)
+print("temp is : ", temp)
+st_out = temp
+print("st_out is : ", st_out)
+print()
+print("fin")
+print()
