a9255f45dc744053db3d293d585d10c913820715
angeleveritt <angel.everitt@gmail.com>
2025-02-24 14:30:38 -0500

    initial

---
+++ various/dictionary_assignment.py
@@ -0,0 +1,20 @@
+# dictionary assignment
+print("Dictionary assignment")
+print()
+count = 0
+
+#def main() :
+#    count = 0
+pattern = "patt"
+print(pattern)
+arr = ['ffafsadasdfmda;dmsa;dsoa;fnewlkwdnkwqndpatt', 'patt', 'pattfhnalsdnsal', 'patt', 'fjoaflasdlasdowqroqnld', 'patt']
+print(arr)
+
+countofpattern = arr.count(pattern)
+count = countofpattern
+print(count)
+#     return (count)
+
+#main()
+
+
