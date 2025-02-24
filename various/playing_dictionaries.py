8e333a7056ea2ca75cb19997e1afa784a20d5748
angeleveritt <angel.everitt@gmail.com>
2025-02-24 13:45:13 -0500

    initial

---
+++ various/playing_dictionaries.py
@@ -0,0 +1,13 @@
+# playing w dictionaries // key then value
+print("playing w dictionaries")
+print()
+dict1 = {"h": "hello", "w": "welcome"} # h and hello are together, w and welcome are together
+print("dict1 looks like this : ", dict1)
+
+print("now we use the keys")
+print("h will return : ", dict1['h'])
+#print("k is not defined so it returns KeyError: 'k' : ", dict1["k"])
+
+print()
+print("fin")
+print()
