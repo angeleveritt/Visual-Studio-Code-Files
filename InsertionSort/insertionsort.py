d69af1c9936c5a9c6f5a789bf749ef3617912215
angeleveritt <angel.everitt@gmail.com>
2025-03-02 17:31:49 -0500

    initial

---
+++ InsertionSort/insertionsort.py
@@ -0,0 +1,28 @@
+# Insertion Sort Assignment - Angelique Everitt
+print()
+print("Insertion Sort Assignment")
+print()
+
+def insertionsort(arr):
+    n = len(arr)
+    
+    if n<= 1 :
+        return # already sorted
+    
+    for i in range(1, n) :             # iterate from second element
+        key = arr[i]                   # store curren element as key to be inserted
+        j = i-1
+        while j>= 0 and key < arr[j] : # move element
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+arr = [12, 11, 13, 5, 6]
+print("unsorted list", arr)
+insertionsort(arr)
+print()
+print("sorted list", arr)
+
+print()
+print("fin")
+print()
