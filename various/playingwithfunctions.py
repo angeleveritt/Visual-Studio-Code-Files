1b7943528d8da4741204644e2d0e07ebe1ea9097
angeleveritt <angel.everitt@gmail.com>
2025-03-03 15:32:29 -0500

    not working

---
+++ various/playingwithfunctions.py
@@ -0,0 +1,45 @@
+# playing w functions
+print()
+print("Playing w functions")
+print()
+
+#def calculate_square(number) :
+#    answer = number ** 3
+#    return answer
+
+#squared_value = calculate_square(10)
+#print(squared_value)
+
+
+print()                                             # for some reason this is returning the result from squared_value above.  I had to # it out.  Now it returns text but no numbers.
+print("recursive functions")
+print()
+def tri_recursion(k) :
+    if(k>0) :
+       result = k+tri_recursion(k-1)
+       print(result)
+    else :
+        result = 0
+    return result
+print()
+print("\n\nRecursion Example Results")
+
+
+
+
+
+
+
+print()
+print("fin")
+print()
+
+
+
+
+
+
+
+print()
+print("fin")
+print()
