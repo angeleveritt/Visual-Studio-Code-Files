c98ab7bb2f4e007ad2e55d04e67fb764a3f8a860
angeleveritt <angel.everitt@gmail.com>
2025-03-04 11:41:58 -0500

    initial

---
+++ various/another_python_file.py
@@ -0,0 +1,14 @@
+# another_python_file.py
+print()
+print("playing w modules")
+print()
+
+import new_module_1
+print()
+print(new_module_1.greeting("Sam"))
+print(new_module_1.square(10))
+
+print()
+print("fin")
+print()
+
