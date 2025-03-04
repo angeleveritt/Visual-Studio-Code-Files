6dc415dd53847e86b66a6809c12772664a7b2378
angeleveritt <angel.everitt@gmail.com>
2025-03-04 11:42:07 -0500

    initial

---
+++ various/new_module_1.py
@@ -0,0 +1,16 @@
+# playing with modules
+print()
+print("Playing with Modules")
+print()
+
+import math
+dir(math)
+print(dir(math))
+
+# new_module_1.py
+
+def greeting(name) :
+    return f"Hello, {name}!"
+
+def square(x) :
+    return x ** 2
