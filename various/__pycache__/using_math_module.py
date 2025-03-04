5d8b25104d0789a16972eb2feba52a9e33684927
angeleveritt <angel.everitt@gmail.com>
2025-03-04 12:03:16 -0500

    initial

---
+++ various/__pycache__/using_math_module.py
@@ -0,0 +1,20 @@
+# assignment Using Python's Math Module - Angelique
+print()
+print("Using Python's Math Module Assignment")
+print()
+
+def main() :
+    import math
+    x = int(input("Enter a number to be calculated to the power of 20 : "))      # working
+    y = 20
+    result = x ** y
+    print("result is : ", result)
+    return result
+
+
+main()
+
+
+print()
+print("fin")
+print()
