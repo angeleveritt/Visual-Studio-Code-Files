d72230ff610cd59cfcd93790123e21a2ebbccd99
angeleveritt <angel.everitt@gmail.com>
2025-03-05 11:55:36 -0500

    shorter better

--- Average/average.py
+++ Average/average.py
@@ -3,23 +3,18 @@ print()
 print("Assignment - Calculate the Average")
 print()
 
-def total() :
-    for i in range(len(ls1)-1) :
-        print(ls1[i])
-        interim = interim + ls1[i]
-        print("The interim total is now : ", interim)
-    return interim
-    
-ls1 = [5, 5, 10, 20, 60]
-length = len(ls1)
-print("The list length is : ", length)                              # this is working
-total(ls1)
-print("The sum of all list elements is : ", interim)
-      
-      
-average = interim / length
-print("The average is : ", average)
+import math
 
+def calculate_average(ls1):
+    if len(ls1) < 1:
+        return 0
+    return sum(ls1) / len(ls1)
+
+ls1 = [5, 5, 10, 20, 60]
+print(ls1)
+
+average = calculate_average(ls1)
+print("The average of the list is : ", average)
 
 print()
 print("fin")
