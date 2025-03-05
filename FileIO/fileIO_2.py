f7901256a0be442547280f893495a98921c3ee13
angeleveritt <angel.everitt@gmail.com>
2025-03-05 16:12:12 -0500

    trying again

---
+++ FileIO/fileIO_2.py
@@ -0,0 +1,16 @@
+# assignment file i/o - Angelique
+print()
+print("Assignment File I/O")
+print()
+
+file = open("test.txt")
+content = file.readlines()
+print("1st line : ", print(content[1]))
+
+
+
+
+
+
+
+
