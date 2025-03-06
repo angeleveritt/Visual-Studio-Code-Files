486d698ff3b6b29d8edf2a899f8afd5cd90636cf
angeleveritt <angel.everitt@gmail.com>
2025-03-05 21:22:18 -0500

    with tutor

--- FileIO/fileIO_2.py
+++ FileIO/fileIO_2.py
@@ -4,14 +4,14 @@ print("Assignment File I/O")
 print()
 
 def read_first_line(file_name):
-    open(file_name)
-    return file_name.readlines
+    with open(file_name) as f:
+        return f.readline()
 
 
 def main():
-    data = read_first_line("test.txt")
-    print(data)
-    return(data)
+    data = read_first_line(r"C:\Users\angel\OneDrive - ABEveritt Inc\8 ABE Training & Courses\00 2025 02 - UWO Python Programming\Visual Studio Code Files\FileIO\test.txt")
+    print(data)             # the little r above tells Windows that this is a raw string and not to be confused by the slashes
+    return data
 
 main()
 
