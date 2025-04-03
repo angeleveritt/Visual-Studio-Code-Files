4c903c8d3710e494afafc76e3f76e5b4d806c6ac
angeleveritt <angel.everitt@gmail.com>
2025-04-03 19:59:07 -0400

    many changes

---
+++ Tutor Lessons/question_answer_lists.py
@@ -0,0 +1,37 @@
+# homework for 2025 03 22
+print()
+print()
+
+questions = [
+    "email address:  ", 
+     "If you would like text notifications during the conference, enter your cell:  ",
+     "Conference badge number:  "
+]
+while True:
+
+    attendee_name = input("Enter your name (lastname, firstname), QUIT to leave:  ")
+    if attendee_name.upper() == "QUIT":         #.upper causes the response to be converted to uppercase
+        break
+
+    #print(questions)          
+    answers = []
+
+    print("Welcome to the ABC Conference")
+    print()
+    print("Please register below")
+
+    for question in questions:    # not a good idea to use a single character variable name.  Less opportunity for misinterpreting.
+        answer = input(question)          
+        answers.append(answer)
+            
+    print("You are registered.  Have an excellent conference!")
+
+
+    number_of_questions = len(questions)
+    for index in range(number_of_questions):    # for index in range(len(questions))
+        print(questions[index], answers[index])  # eventually, when multiple users, you will have a nested list for the answers
+
+    # eventually the questions and answers would be combined into a dictionary and stored in a file
+    # might use the badge number as a key 
+
+    # he will take me through some teaching next week
