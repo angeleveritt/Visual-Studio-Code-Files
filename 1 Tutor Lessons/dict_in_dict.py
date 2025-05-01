0f1b56115cd7c019b9f74ec7be2537709c16a0ad
angeleveritt <angel.everitt@gmail.com>
2025-05-01 18:08:21 -0400

    many changes and I've broken it

---
+++ 1 Tutor Lessons/dict_in_dict.py
@@ -0,0 +1,145 @@
+# creating dictionaries
+# creating dictionaries in dictionaries
+# https://pythonguides.com/dictionaries/
+
+print()
+print("1111111111111111111111111111111")    # Andy's example
+print("create dictionary booking using curly brackets and incl extra attributes --> customer_name: name: Customer name, value: None")
+print("nested dictionaires, I believe")
+booking = {                                                               # method 1
+    "customer_name": {"name": "Customer name", "value": None},
+    "weight_kg": {"name": "Weight in kg", "value": None},
+    "volume_m3": {"name": "Volume in cubic meters", "value": None},
+}
+for field_name, value in booking.items():    # how I like to print things
+    booking[field_name]["value"] = input(booking[field_name]["name"] + ": ")
+print(booking)
+print()
+for key, value in booking.items():
+        #print("key=%s, value=%s" % (key, value))
+        print(key, value)    
+print()
+print("trying to print specific item")
+print (booking["volume_m3"])
+print()
+# question for Andy:  why use the dict() constructor ever
+print()
+print("22222222222222222222222222222222222222")
+print("this doesn't use curly brackets --> customer = dict([('name', 'John Smith') ...    ])")
+print()
+my_dict = dict()                                                         # method 2 - no curly brackets but it's still a dictionary?
+customer = dict([
+    ('name', 'John Smith'),
+    ('age', 35),
+    ('city', 'New York'),
+    ('is_active', True)
+])
+print("customer: ", customer)
+print(customer)
+for key, value in customer.items():
+        #print("key=%s, value=%s" % (key, value))
+        print(key, value)    
+print()
+print("accessing a value --> print(customer['name'])")
+print(customer['name'])
+print()
+
+
+
+print("333333333333333333333333333333333333333333")
+print()
+# ask Andy about this too
+print()
+print("creating a dictionary using {x:x*x for x in range(1, 6)}")
+squares = {x:x*x for x in range(1, 6)}
+print(squares)
+for key, value in squares.items():
+        #print("key=%s, value=%s" % (key, value))
+        print(key, value)    
+print()
+print("trying to print specific item")
+print (squares[3])
+print()
+print("4444444444444444444444444444444444444")
+print("creating a dictionary from lists")
+cities = ["New York", "Los Angeles", "Chicago"]
+populations = [8804190, 3898747, 2746388]
+city_pops = {city: pop for city, pop in zip(cities, populations)}                     # is zip a function?
+print(city_pops)
+print()
+for key, value in city_pops.items():
+        #print("key=%s, value=%s" % (key, value))
+        print(key, value)    
+print()
+print("trying to print specific item")
+print (city_pops["Chicago"])
+print()
+print("55555555555555555555555555")
+print()
+
+# what is get() about?
+print()
+print("get(), etc")
+print()
+product = {
+       "name": "Laptop",
+       "price": 999.99,
+       "brand": "TechPro",
+       "in_stock": True
+}
+
+print("get all keys")
+print(list(product.keys()))
+print()
+print("get all values")
+print(list(product.values()))
+print()
+print("get all key-value pairs as tuples")
+print(list(product.items()))
+print()
+print("6666666666666666666666666666666666666666")
+# what is setdefault() about
+# what is creating a dictionary with conditionals about and does that apply to my air shipping cost part of the packages assignment?
+# nested dictionaries
+print()
+print("nested dictionaries, again?")
+print()
+employees = {
+       'E001':{
+              "name":"James Smith",
+              "department": "Sales",
+              "salary": 65000,
+              "contact": {
+                     "email": "james@example.com", 
+                     "phone": "555-555-1234"
+
+              }
+       },
+       'E002':{
+              "name": "Lisa Johnson",
+              "department": "Marketing", 
+              "salary": "70000",
+              "contact": {
+                     "email": "lisa@example.com",
+                     "phone": "555-555-6565"
+              }
+       }
+}
+
+print(employees)
+print(list(employees.items()))
+print()
+for key, value in employees.items():
+        #print("key=%s, value=%s" % (key, value))
+        print(key, value)    
+        
+print()
+print()
+
+
+print("777777777777777777777777777777777777777777777777777777777")
+print()
+a = "2"
+b = 4
+c = int(a)
+print(a, b, c)
---
+++ 1 Tutor Lessons/packages3.py
@@ -0,0 +1,138 @@
+# Assignment - Packages
+# Classes are not necessary for smaller assignments/applications - there is a lot of overhead and intellectual demand
+# this is between 100 and 200 lines of code // Classes are going out of favour, in favour of Functional Programming
+# Dictionary rather Class here
+# have to figure out what the separate dictionary for reference information would hold
+
+print() 
+
+"""def get_input():                                                                 #originally all the input and declared variables were together
+    shipment_quote = {}
+    print()
+    shipment_quote["customer_name"] = input("Customer name: ")    
+    shipment_quote["weight_kg"] = int(input("Package weight in kilograms: "))
+    shipment_quote["cubic_meters"] = int(input("Cubic meters of package: ")) #I have them doing the math outside of program
+    shipment_quote["dangerous"] = input("Dangerous goods? (y/n): ")
+    return shipment_quote        """                                                # broke it up so I'm not asking for more info than I need to establish can_ship
+
+def get_input():
+    shipment_quote = {                                                              # nested dictionary version
+        "customer_name": {"name": "Customer name", "value": None},
+        "weight_kg": {"name": "Package weight (kg)", "value": 0},                # how do I add the Int here // changed from None to 0 to get integer
+        "cubic_meters": {"name": "Package volume (cubic meters)", "value": 0},   # how do I add the Int here // changed from None to 0 to get integer
+        "dangerous": {"name": "Dangerous goods? (y/n)", "value": None}              # will need to translate to True False
+    }
+    for field_name, value in shipment_quote.items():    
+        shipment_quote[field_name]["value"] = input(shipment_quote[field_name]["name"] + ": ") # cannot bounce out before dangerous if too heavy or too large
+    return shipment_quote
+
+def evaluate_package(shipment_quote):
+    (shipment_quote["weight_kg"]) = int(shipment_quote["weight_kg"])                        # the error is in the area but I think I've written this correctly
+    (shipment_quote["cubic_meters"]) = int(shipment_quote["cubic_meters"])
+    if ((shipment_quote["weight_kg"] >= 10) or (shipment_quote["cubic_meters"] >= 125)):          # too heavy, too voluminous
+        shipment_quote["can_ship"] = "False" 
+        print("We cannot ship your package because it is either more than 10kg or more than 125 cubic meters.")                
+        print()
+                                 # exit() quit() both bounce me entirely out of the program and I cannot use break or continue here
+    else: 
+        shipment_quote["can_ship"] = "True"                    
+    return(shipment_quote)
+
+def declare_variables(shipment_quote):
+   
+    shipment_quote["can_ship"] = None                                       # confirm that this retains the items input above
+    shipment_quote["air_possible"] = None                                   # use None for defaults for safety and debugging
+    shipment_quote["ship_ground_nonurgent_cost"] = None                     # None is a value that means I don't have a value yet = built into Python, like True or False
+    shipment_quote["ship_ground_urgent_cost"] = None
+    shipment_quote["ship_ocean_cost"] = None
+    shipment_quote["ship_air_cost_kg"] = None
+    shipment_quote["ship_air_cost_cm"] = None                          
+    return shipment_quote
+
+
+
+"""def validate_data(shipment_quote):                                          # not working but hasn't broken anything else
+    print("testing if validate_data is ever opened")
+    if ((shipment_quote["dangerous"] == "Y") or (shipment_quote["dangerous"]=="y") or (shipment_quote["dangerous"]=="Yes") or (shipment_quote["dangerous"]=="yes") or (shipment_quote["dangerous"]=="YES")):
+        shipment_quote["dangerous"] = True
+    if ((shipment_quote["dangerous"] == "N") or (shipment_quote["dangerous"]=="n") or (shipment_quote["dangerous"]=="No") or (shipment_quote["dangerous"]=="no") or (shipment_quote["dangerous"]=="NO")):
+        shipment_quote["dangerous"] = False
+    if ((shipment_quote["dangerous"] != True) or (shipment_quote["dangerous"] != False)):
+        print("Invalid value for Dangerous.")
+    return(shipment_quote)"""
+
+def calculate_ocean(shipment_quote):                                                  # ocean
+    if shipment_quote["can_ship"] == "True":
+        shipment_quote["ship_ocean_cost"] = 30                                         # cost for ocean flat rate
+                                                                                                          
+    return(shipment_quote)    
+
+
+def calculate_air(shipment_quote):                                                            # air
+    if ((shipment_quote["dangerous"] == "y") or (shipment_quote["can_ship"] == "False")):    # removed if ((shipment_quote["heavy"] == "y") or
+        shipment_quote["air_possible"] = "False"
+    else:
+        shipment_quote["air_possible"] = "True"
+    if shipment_quote["air_possible"] == "True":                                               # but this part is working
+        shipment_quote["ship_air_cost_kg"] = (shipment_quote["weight_kg"] * 10)          # cost for air weight
+        shipment_quote["ship_air_cost_cm"] = (shipment_quote["cubic_meters"] * 20)       # cost for air volume
+    return(shipment_quote)
+
+def calculate_ground(shipment_quote):                                                          # ground
+    if shipment_quote["can_ship"] == "True":
+        shipment_quote["ship_ground_nonurgent_cost"] = 25                                # cost for ground if not urgent flat rate
+        shipment_quote["ship_ground_urgent_cost"] = 45                                   # cost for ground if urgent flat rate
+    return(shipment_quote)    
+                  
+def fmi(shipment_quote):                                                             # prints it the way I prefer
+    print("Shipment Quote Details")                                                  # complete dictionary
+    print("-----------------------")
+    for key, value in shipment_quote.items():
+        #print("key=%s, value=%s" % (key, value))
+        print(key, value)    
+    return(shipment_quote)
+                                                          
+def main():                                      
+    while True:
+        print()
+        print("Shipment System")                               # not sure if this actually goes at the top or is a function that is called near the bottom
+        print("Enter 1 to get a shipment quote.")              # I don't have this doing anything yet other than taking input
+        #print("Enter 2 to update customer information.")
+        print("Enter 3 to quit.")
+        action = input("What would you like to do? ")
+
+        if action == "1":
+            shipment_quote = get_input()
+            shipment_quote = evaluate_package(shipment_quote)
+            if shipment_quote["can_ship"] == "False":
+                continue                                        # it doesn't ask if the pkg is dangerous if it's already too big.  Could maybe do this after each answer.
+            shipment_quote = declare_variables(shipment_quote)
+            shipment_quote = calculate_ocean(shipment_quote)
+            shipment_quote = calculate_air(shipment_quote)
+            shipment_quote = calculate_ground(shipment_quote)
+            print()
+            shipment_quote = fmi(shipment_quote)
+        if action == "3":
+            break
+
+if __name__ == "__main__":                        # name is special type of variable -->  called a dunder (double-underscore) // allows me to import from here to other programs 
+    main()                                        # getting error here now 
+                                                # writing this way means then entire .py file is now a module rather than just a script
+
+
+print()
+print("fin")
+print
+
+
+
+# can we include the statements like "We cannot ship this package because too heavy and too voluminous" as key value pairs.  Key could be "message"
+# to dos:
+    # play w dictionary in dictionary - sample in tutor folder
+    # change my y/n to True/False?? not sure if this is just part of what he has us doing next wk
+    # We will rework my program next week
+    # 
+    # maybe:
+    # > error msgs
+    # > air cost will be x -- if air weight cost is larger than air volume cost, then print x.  Do I create a variable to hold the result of comparison?  Do I add a message in the key value pairs?
+
